56122b602614d322c08f731de22cf0fde2c853f1
Gary Grossman <ggrossman@zendesk.com>
2014-07-29 13:39:49 -0700

    Make the invalid UTF8 test a no-op if Ruby 1.8 is used (strings not encoding-aware)
    whoops

--- test/credit_card_sanitizer_test.rb
+++ test/credit_card_sanitizer_test.rb
@@ -48,8 +48,10 @@ class CreditCardSanitizerTest < MiniTest::Test
       end
 
       it "doesn't fail if the text contains invalid utf-8 characters" do
-        invalid_characters = "你好 12 345123 451234 8 \255there"
-        assert_equal "你好 12 3451XX XXX234 8 \ufffdthere", @sanitizer.sanitize!(invalid_characters)
+        if ''.respond_to?(:encoding)
+          invalid_characters = "你好 12 345123 451234 8 \255there"
+          assert_equal "你好 12 3451XX XXX234 8 \ufffdthere", @sanitizer.sanitize!(invalid_characters)
+        end
       end
     end
 
